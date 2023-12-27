70f9ef09c660b9070a9abecc4ce9ea5d6230a19d
Arnaud Sautaux <arnaud.sautaux@gmail.com>
2023-12-27 17:03:03 +0100

    player movement

---
+++ scenes/player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+# https://kidscancode.org/godot_recipes/4.x/2d/grid_movement/index.html
+# I will implement the script for the player
+# https://youtu.be/HmnwNadwHWI?si=S5ldGgg4OL4NJTeL&t=390
+var tile_size = 64
+var inputs = {
+	"ui_right": Vector2.RIGHT,
+	"ui_left": Vector2.LEFT,
+	"ui_up": Vector2.UP,
+	"ui_down": Vector2.DOWN,
+}
+
+func _ready():
+	position = position.snapped(Vector2.ONE * tile_size)
+	position = position + Vector2.ONE * tile_size/2
+
+func _unhandled_input(event):
+	for dir in inputs.keys():
+		if event.is_action_pressed(dir):
+			move(dir)
+
+func move(dir):
+	position = position + inputs[dir] * tile_size
